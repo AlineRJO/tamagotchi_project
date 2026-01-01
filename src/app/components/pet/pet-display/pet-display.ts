32a0f3964dbd68ade23b28f06d5ae8b59aab6dae
Aline Rabello <aline.rabello.jose@gmail.com>
2026-01-01 14:27:18 -0300

    chore(pet-display): create component

---
+++ src/app/components/pet/pet-display/pet-display.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet-display.html',
+  styleUrl: './pet-display.scss',
+})
+export class PetDisplay {
+
+}
